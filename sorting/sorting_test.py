5100d8f6aab0a58b30dbd459fb9e9a30aad7ff6f
szylec <sdfrew@gmail.com>
2018-05-16 09:35:29 +0200

    Refactoring and adding insertion sort

--- sorting/sorting_test.py
+++ sorting/sorting_test.py
@@ -1,39 +1,45 @@
 import sys
 import unittest
 import sorting
+from unittest import mock
 
 class TestSorting(unittest.TestCase):
 
-	def setUp(self):
-		self.list_to_sort = [3,8,3,6,1]
+	@classmethod
+	def setUpClass(cls):
+		cls.sorting = sorting.Sorting()
+		cls.mock_list_to_sort = [3, 8, 3, 6, 1]
+		cls.mock_sorted_list = [1, 3, 3, 6, 8]
 
 	def test_count_number_of_elements(self):
 		self.assertEqual(
-			sorting.count_number_of_elements(self.list_to_sort), 5)
+			self.sorting.count_number_of_elements(self.mock_list_to_sort), 5)
 
 	def test_prepare_to_print(self):
-		string_list = sorting.prepare_to_print(self.list_to_sort)
+		string_list = self.sorting.prepare_to_print(self.mock_list_to_sort)
 		self.assertEqual(string_list[0], "[")  # If first char is "["
 		self.assertEqual(string_list[-1], "]")  # If last char is "]"
 
-	def test_calculate_and_show_results(self):
-		"""To be defined."""
-		pass
+	@mock.patch.object(sorting.Sorting, "bubble_sort")
+	def test_execute_and_show_results(self, mock_bubble_sort):
+		self.sorting.execute_and_show_results(
+			'bubble', mock_bubble_sort, self.mock_list_to_sort)
+		self.assertTrue(mock_bubble_sort.called)
 
 	def test_bubble_sort(self):
-		sorted_list = sorting.bubble_sort(
-			self.list_to_sort)
-		self.assertEqual(sorted_list, [1,3,3,6,8])
+		sorted_list = self.sorting.bubble_sort(
+			self.mock_list_to_sort)
+		self.assertEqual(sorted_list, self.mock_sorted_list)
 
 	def test_selection_sort(self):
-		sorted_list = sorting.selection_sort(
-			self.list_to_sort)
-		self.assertEqual(sorted_list, [1,3,3,6,8])
+		sorted_list = self.sorting.selection_sort(
+			self.mock_list_to_sort)
+		self.assertEqual(sorted_list, self.mock_sorted_list)
 
 	def test_insertion_sort(self):
-		sorted_list = sorting.insertion_sort(
-			self.list_to_sort)
-		self.assertEqual(sorted_list, [1,3,3,6,8])
-
+		sorted_list = self.sorting.insertion_sort(
+			self.mock_list_to_sort)
+		self.assertEqual(sorted_list, self.mock_sorted_list)
+		
 if __name__ == "__main__":
 	unittest.main()
